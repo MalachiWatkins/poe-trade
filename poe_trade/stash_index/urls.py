7f480f37d30ff398b688aa0f161cf1e60018519a
Malachi <malachi.watkins3535@gmail.com>
2020-12-21 09:16:24 +0000

    WIP

--- poe_trade/stash_index/urls.py
+++ poe_trade/stash_index/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from stash_index import views
 from django.conf.urls import include
 from django.conf.urls import url
+
 urlpatterns = [
     # path('', views., name=''),
     path('', views.home, name='home'),
     path('currencyview', views.currencyView, name='currencyView'),
-    path('cardview', views.cardView, name='cardView'),
+    # path('cardview', views.cardView, name='cardView'),
+    # path('test', views.test.as_view()),
     url(r'', views.bootstrap4_index, name="base"),
 
 ]
--- poe_trade/stash_index/views.py
+++ poe_trade/stash_index/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import View
 from configparser import ConfigParser
 import pymongo
 from pymongo import MongoClient
@@ -8,9 +9,8 @@ import json
 import time
 import random
 import re
+from django.http import HttpResponse
 from Atlas_Connection import Atlas_Connect
-
-
 cluster = MongoClient(Atlas_Connect)
 Itemdb = cluster["POE_DOCS"]
 
@@ -34,20 +34,55 @@ def home(request):
 
 def currencyView(request):  # Gets all currency data from the db and sends it to frontend
     context = {}
-    currencyCollection = Itemdb["Currency"]  # gets currency collection
-    currency_post_list = []  # a list to store all the currency related posts
-    for doc in currencyCollection.find():  # finds all the currency documents and appends them to a list
-        currency_post_list.append(doc)
-    # sends the post data to frontend
-    context['currencyPost'] = currency_post_list
+    currencyCollection = Itemdb["Currency"]
+    currency_raw_post = []
+    processed_data = []
+    for doc in currencyCollection.find():
+        currency_raw_post.append(doc)
+    x = 0
+    while x < len(currency_raw_post):
+        single_post = currency_raw_post[x]
+        post_data = {
+            "icon": single_post['icon'],
+            "accountname": single_post['accountName'],
+            "type": single_post['typeLine'],
+        }
+        print()
+        processed_data.append(post_data)
+        #     post_data = {
+        #         "icon": single_post['icon'],
+        #         "accountname": single_post['accountName'],
+        #         "type": single_post['typeLine'],
+        #         "stack": single_post['stackSize'],
+        #         "price": single_post['note'],
+        #         "ilvl": single_post['ilvl'],
+        #     }
+        #
+        #     processed_data.append(post_data)
+        #     print(processed_data)
+        x += 1
+        #
+    context['data'] = processed_data
     return render(request, 'currency_view.html', context)
 
+#
+# def cardView(request):
+#     context = {}
+#     cardsCollection = Itemdb["Cards"]
+#     card_post_list = []
+#     for doc in cardsCollection.find():
+#         card_post_list.append(doc)
+#     context['cardPost'] = card_post_list
+#     return render(request, 'card_view.html', context)
 
-def cardView(request):
-    context = {}
-    cardsCollection = Itemdb["Cards"]
-    card_post_list = []
-    for doc in cardsCollection.find():
-        card_post_list.append(doc)
-    context['cardPost'] = card_post_list
-    return render(request, 'card_view.html', context)
+#
+#
+#
+# class test(View):
+#
+#     def get(self, request):
+#         return currencyView(request)
+#         # return render(request, '')
+#
+#     def post(self, request):
+#         return HttpResponse('Class based view')
