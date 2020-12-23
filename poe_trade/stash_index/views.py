c56d81bf2d1e4f14f3a5cb6ccbf7f64bf13aabaf
Malachi <malachi.watkins3535@gmail.com>
2020-12-23 01:57:53 +0000

    created a function for all data processing

--- poe_trade/stash_index/views.py
+++ poe_trade/stash_index/views.py
@@ -13,7 +13,7 @@ from django.http import HttpResponse
 from Atlas_Connection import Atlas_Connect
 cluster = MongoClient(Atlas_Connect)
 Itemdb = cluster["POE_DOCS"]
-
+currencyCollection = Itemdb["Currency"]
 accessoriesCollection = Itemdb["accessories"]
 gemsCollection = Itemdb["gems"]
 jewelsCollection = Itemdb["Jewels"]
@@ -21,9 +21,30 @@ mapsCollection = Itemdb["Maps"]
 weaponsCollection = Itemdb["weapons"]
 armourCollection = Itemdb["armour"]
 flaskCollection = Itemdb["flasks"]
-
+context = {}
 # TODO: finish other functions (dont worrie about the html just create them)
 # get home paage working similar to the offical poe trade site
+
+
+def data_processing(type, data_list):
+    global processed_data
+    raw_post = []
+    processed_data = []
+    for doc in type.find():
+        raw_post.append(doc)
+    x = 0
+    while x < len(raw_post):
+        single_post = raw_post[x]
+        post_data = {}
+        for key in data_list:
+            try:
+                post_data[key] = single_post[key]
+            except KeyError:
+                pass
+        processed_data.append(post_data)
+        x += 1
+
+    return
 
 
 def bootstrap4_index(request):
@@ -36,28 +57,9 @@ def home(request):
 
 
 def currencyView(request):  # Gets all currency data from the db and sends it to frontend
-    context = {}
-    currencyCollection = Itemdb["Currency"]
-    currency_raw_post = []
-    processed_data = []
-    for doc in currencyCollection.find():
-        currency_raw_post.append(doc)
-    data_list = ['icon', 'accountName',
-                 'typeLine', 'stackSize', 'note', 'ilvl', 'stashname', 'stashid', 'x', 'y']
-    x = 0
-
-    while x < len(currency_raw_post):
-        single_post = currency_raw_post[x]
-        post_data = {}
-        for key in data_list:
-            try:
-                post_data[key] = single_post[key]
-            except KeyError:
-                pass
-        processed_data.append(post_data)
-
-        x += 1
-
+    datalist = ['icon', 'accountName', 'typeLine',
+                'stackSize', 'note', 'ilvl', 'stashname', 'stashid', 'x', 'y']
+    data_processing(type=currencyCollection, data_list=datalist)
     context['data'] = processed_data
     return render(request, 'currency_view.html', context)
 
