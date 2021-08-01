cea090e3bcfd78e89901105b6fba0aee7a3bbffd
Malachi Watkins <malachiwatkins@Haileys-Air.attlocal.net>
2021-08-01 11:28:31 -0500

    WIP

--- poe_trade/stash_index/forms.py
+++ poe_trade/stash_index/forms.py
@@ -43,8 +43,25 @@ Currency_choice = (
     ('Perandus_coin', ' Perandus Coin'),
     ('Rogue_marker', 'Rogue Marker'),
     ('silver_coin', 'Silver Coin'),
-
-
+)
+rarity_choices = (
+    ('Rare', 'Rare'),
+    ('Rare', 'Rare'),
+    ('Rare', 'Rare'),
+    ('Rare', 'Rare'),
+    ('Rare', 'Rare'),
+)
+item_category = (
+    ('Rare', 'Any'),
+    ('Rare', 'Rare'),
+    ('Rare', 'Rare'),
+    ('Rare', 'Rare'),
+    ('Rare', 'Rare'),
+)
+identified = (
+    ('any', 'any'),
+    ('ture', 'ture'),
+    ('False', 'False'),
 )
 
 
@@ -55,33 +72,31 @@ class buyform(forms.Form):
     collection_type = forms.CharField(label='What kind of item is this?',
                                       widget=forms.Select(choices=COLLECTION_CHOICES))
 
-    item_name = forms.CharField(label='Item name', widget=forms.TextInput(
-        attrs={'placeholder': 'Name of item'}))
+    ############################################
+    ################Type Filter###################
+    ############################################
 
-    Item_price_currency = forms.CharField(label='Item Price',
-                                          widget=forms.Select(choices=Currency_choice))
+    Item_rarity = forms.CharField(
+        widget=forms.Select(choices=rarity_choices))
+    Item_category = forms.CharField(
+        widget=forms.Select(choices=item_category))
+    ############################################
+    ############# Weapon Filter ##################
+    ############################################
 
-    Item_price_quantity = forms.CharField(label='Item name', max_length=100, widget=forms.TextInput(
-        attrs={'placeholder': '#'}))
-    Item_price_quantity.widget.attrs.update(size='2')
-    accountname = forms.CharField(label='account name', max_length=100, widget=forms.TextInput(
-        attrs={'placeholder': 'Account Name'}))
+    ############################################
+    ############### Armour #####################
+    ############################################
 
-    Is_Identified = forms.CharField(label='', max_length=100, widget=forms.CheckboxInput(
-        attrs={'placeholder': 'Is Item Identified'}))
-
-    Item_level = forms.CharField(label='', max_length=100, widget=forms.TextInput(
-        attrs={'placeholder': '#'}))
-    Item_level.widget.attrs.update(size='2')
-    # accountname = forms.CharField(widget=forms.Textarea(attrs={'rows': 3}))
-
-    Item_name = forms.CharField(label='iname', max_length=100, widget=forms.TextInput(
-        attrs={'placeholder': 'Item Name'}))
-
-    Item_base = forms.CharField(label='ibase', max_length=100, widget=forms.TextInput(
-        attrs={'placeholder': 'Item Base'}))
+    ############################################
+    ############### Sockets #####################
+    ############################################
     Item_sockets = forms.CharField(label='isockets', max_length=100, widget=forms.TextInput(
         attrs={'placeholder': 'EX: B-B-B R-R'}))
+
+    ############################################
+    ############# Requirements ###################
+    ############################################
     Item_req_Lvl = forms.CharField(label='ireq', max_length=100, widget=forms.TextInput(
         attrs={'placeholder': 'LVL'}))
     Item_req_Lvl.widget.attrs.update(size='3')
@@ -94,63 +109,31 @@ class buyform(forms.Form):
     Item_req_Dexterity = forms.CharField(label='ireqdex', max_length=100, widget=forms.TextInput(
         attrs={'placeholder': 'DEX'}))
     Item_req_Dexterity.widget.attrs.update(size='3')
- # 1
-  # 1
-   # 1
-    # 1
-    # 1
-    # 1
-    # 1
-    # 1
-    # 1
-    # 1
-    # 1
-    # 1
-    # 1
-    # 1
-    # 1
-    # 1
-    # 1
-    # 1
-    # 1
-    # 1
-    # 1
-    # 1
-    # 1
-    # 1
-    # 1
-    # 1
-    # 1
-    # 1
-    # 1
-    # 1
-    # 1
-    # 1
-    # 1
 
-    # 1
-    # 1
-    # 1
-    # 1
-    # 1
-    # 1
-    rarity_choices = (
-        ('Rare', 'Rare'),
-        ('Rare', 'Rare'),
-        ('Rare', 'Rare'),
-        ('Rare', 'Rare'),
-        ('Rare', 'Rare'),
-    )
-    Item_category = (
-        ('Rare', 'Rare'),
-        ('Rare', 'Rare'),
-        ('Rare', 'Rare'),
-        ('Rare', 'Rare'),
-        ('Rare', 'Rare'),
-    )
-    type_filter = forms.CharField(label='', max_length=100, widget=forms.CheckboxInput(
-        attrs={'placeholder': 'Is Item Identified'}))
-    Item_rarity = forms.CharField(
-        widget=forms.Select(choices=rarity_choices))
-    Item_category = forms.CharField(
-        widget=forms.Select(choices=Item_category))
+    ############################################
+    ################ Map ######################
+    ############################################
+
+    ############################################
+    ############## MISC #######################
+    ############################################
+    Item_level = forms.CharField(label='', max_length=100, widget=forms.TextInput(
+        attrs={'placeholder': '#'}))
+    Item_level.widget.attrs.update(size='2')
+
+    Is_identified = forms.CharField(
+        widget=forms.Select(choices=identified))
+    ############################################
+    ################ Trade ######################
+    ############################################
+    accountname = forms.CharField(label='account name', max_length=100, widget=forms.TextInput(
+        attrs={'placeholder': 'Account Name'}))
+    Item_price_currency = forms.CharField(label='Item Price',
+                                          widget=forms.Select(choices=Currency_choice))
+
+    Item_price_quantity = forms.CharField(label='Item name', max_length=100, widget=forms.TextInput(
+        attrs={'placeholder': '#'}))
+    Item_price_quantity.widget.attrs.update(size='2')
+    ############################################
+    ################## Mods ####################
+    ############################################
